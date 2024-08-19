f58d08833ca97d29eb5927a27cc9eea16a2e0f39
Donald Gray <donald.gray@digirati.com>
2024-08-19 11:23:32 +0100

    Configure serilog

--- src/DigitalPreservation/Preservation.API/Program.cs
+++ src/DigitalPreservation/Preservation.API/Program.cs
@@ -1,6 +1,42 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Serilog;
 
-app.MapGet("/", () => "Preservation: Hello World!");
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
 
-app.Run();
+Log.Information("Application starting..");
+
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+    builder.Host.UseSerilog((hostContext, loggerConfiguration)
+        => loggerConfiguration
+            .ReadFrom.Configuration(hostContext.Configuration)
+            .Enrich.FromLogContext()
+            .Enrich.WithCorrelationId(addValueIfHeaderAbsence: true));
+    
+    builder.Services.AddHttpContextAccessor();
+    
+    var app = builder.Build();
+    app
+        .UseSerilogRequestLogging()
+        .UseForwardedHeaders();
+    
+    app.MapGet("/", () => "Preservation: Hello World!");
+
+    app.Run();
+}
+catch (HostAbortedException)
+{
+    // No-op - required when adding migrations,
+    // See: https://github.com/dotnet/efcore/issues/29809#issuecomment-1345132260
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Unhandled exception on startup");
+}
+finally
+{
+    Log.Information("Shut down complete");
+    Log.CloseAndFlush();
+}
