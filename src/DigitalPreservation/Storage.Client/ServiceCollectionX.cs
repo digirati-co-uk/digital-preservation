aaab383a8d160a8042f3f1fdcadab6717e68bc40
Donald Gray <donald.gray@digirati.com>
2024-08-20 17:49:31 +0100

    AddStorageClient configures IOptions

--- src/DigitalPreservation/Storage.Client/ServiceCollectionX.cs
+++ src/DigitalPreservation/Storage.Client/ServiceCollectionX.cs
@@ -1,5 +1,6 @@
 ﻿using DigitalPreservation.Core.Web;
 using DigitalPreservation.Core.Web.Handlers;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 
@@ -11,11 +12,13 @@ public static class ServiceCollectionX
     /// Add and configure <see cref="IStorageApiClient"/> to service collection 
     /// </summary>
     /// <param name="serviceCollection">Current <see cref="IServiceCollection"/> object</param>
+    /// <param name="configuration">Current <see cref="IConfiguration"/> object</param>
     /// <param name="componentName">Calling component name, used for "x-requested-by" header</param>
     /// <returns>Modified service collection</returns>
     public static IServiceCollection AddStorageClient(this IServiceCollection serviceCollection,
-        string componentName)
+        IConfiguration configuration, string componentName)
     {
+        serviceCollection.Configure<StorageOptions>(configuration.GetSection(StorageOptions.Storage));
         serviceCollection
             .AddTransient<TimingHandler>()
             .AddHttpClient<IStorageApiClient, StorageApiClient>((provider, client) =>
