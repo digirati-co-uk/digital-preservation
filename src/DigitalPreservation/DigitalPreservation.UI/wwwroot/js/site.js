c1d5857214aaad8483fb873d9816128cd1b2b0d1
tomcrane <tom@nanoplanet.com>
2025-01-10 14:40:43 +0000

    Add single-submit behaviour to all forms

--- src/DigitalPreservation/DigitalPreservation.UI/wwwroot/js/site.js
+++ src/DigitalPreservation/DigitalPreservation.UI/wwwroot/js/site.js
@@ -1,3 +1,20 @@
 ﻿
 const permittedSlugString = "abcdefghijklmnopqrstuvwxyz0123456789.-_";
 const permittedSlugChars = permittedSlugString.split('');
+
+document.querySelectorAll('form.single-submit').forEach(form => {
+    form.addEventListener('submit', function(event) {
+        if (this.dataset.submitted && this.dataset.submitted === "submitted") {
+            event.preventDefault();
+            return;
+        }
+        this.dataset.submitted = "submitted";
+        this.querySelectorAll('button, input[type="submit"]').forEach(button => {
+            button.disabled = true;
+        });
+        // Not all submit buttons are descendants of their forms
+        if(event.submitter){
+            event.submitter.disabled = true;
+        }
+    });
+});
