cfcd9fa6668ab0c4052d58d8de128306736737fa
tomcrane <tom@nanoplanet.com>
2025-03-13 12:15:56 +0000

    remove gino from iiif-builder

--- src/iiif-builder/app/db.py
+++ src/iiif-builder/app/db.py
@@ -1,28 +1,129 @@
 from datetime import datetime
+import psycopg
 
-from gino import Gino
+from app import settings
 
-db = Gino()
-
-class ArchivalGroupActivity(db.Model):
+class ArchivalGroupActivity:
     """
     A row is created for every Activity Stream event read by the system
     """
-    __tablename__ = "archival_group_activity"
+    def __init__(self,
+                 id_:int=0,
+                 activity_end_time:datetime=None,
+                 archival_group_uri:str=None,
+                 activity_type:str=None,
+                 id_service_pid:str=None,
+                 catalogue_api_uri:str=None,
+                 public_manifest_uri:str=None,
+                 internal_public_manifest_uri:str=None,
+                 internal_api_manifest_uri:str=None,
+                 started:datetime=None,
+                 finished:datetime=None,
+                 error_message:str=None
+                 ):
+        self.id_ = id_
+        self.activity_end_time = activity_end_time
+        self.archival_group_uri = archival_group_uri
+        self.activity_type = activity_type
+        self.id_service_pid = id_service_pid
+        self.catalogue_api_uri = catalogue_api_uri
+        self.public_manifest_uri = public_manifest_uri
+        self.internal_public_manifest_uri = internal_public_manifest_uri
+        self.internal_api_manifest_uri = internal_api_manifest_uri
+        self.started = started
+        self.finished = finished
+        self.error_message = error_message
 
-    id = db.Column(db.Integer(), primary_key=True) # TODO - how do you make this auto-increment?
-    activity_end_time = db.Column(db.DateTime(timezone=False), nullable=True)
-    archival_group_uri = db.Column(db.String, nullable=False)
-    activity_type = db.Column(db.String, nullable=False) # Create, Update etc
-    catalogue_api_uri = db.Column(db.String, nullable=True)
-    public_manifest_uri = db.Column(db.String, nullable=True) # may not be for IIIF use
-    api_manifest_uri = db.Column(db.String, nullable=True) # may not be for IIIF use
-    started = db.Column(db.DateTime(timezone=False), nullable=True)
-    finished = db.Column(db.DateTime(timezone=False), nullable=True)
-    error_message = db.Column(db.String, nullable=True)
 
     @staticmethod
     async def get_latest_end_time() -> datetime.datetime:
-        sql = "SELECT max(activity_end_time) FROM archival_group_activity"
-        result = await db.one(db.text(sql))
-        return result
+        with psycopg.connect(settings.POSTGRES_CONNECTION) as conn:
+            with conn.cursor() as cur:
+                result = cur.execute("SELECT max(activity_end_time) FROM archival_group_activity").fetchone()[0]
+                if result is None:
+                    return datetime.datetime(2000, 1, 1)
+                return result
+
+
+    @classmethod
+    async def new_activity(cls, activity_end_time, archival_group_uri, activity_type)-> 'ArchivalGroupActivity':
+        with psycopg.connect(settings.POSTGRES_CONNECTION) as conn:
+            with conn.cursor() as cur:
+                sql = ("INSERT INTO archival_group_activity "
+                       "(activity_end_time, archival_group_uri, activity_type, started) "
+                       "VALUES (%s, %s, %s, %s) "
+                       "RETURNING id")
+                values = (activity_end_time, archival_group_uri, activity_type, datetime.datetime.now())
+                new_id = cur.execute(sql, values).fetchone()[0]
+                return ArchivalGroupActivity.get_from_id(new_id)
+
+
+    @staticmethod
+    def get_from_id(new_id:int)-> 'ArchivalGroupActivity':
+        with psycopg.connect(settings.POSTGRES_CONNECTION) as conn:
+            with conn.cursor() as cur:
+                sql = ("SELECT id, activity_end_time, archival_group_uri, activity_type, "
+                       "id_service_pid, catalogue_api_uri, public_manifest_uri, "
+                       "internal_public_manifest_uri, internal_api_manifest_uri, "
+                       "started, finished, error_message "
+                       "FROM archival_group_activity WHERE id = %s")
+                result = cur.execute(sql, new_id).fetchone()
+                if result is None:
+                    return None
+                row = result[0]
+                return ArchivalGroupActivity(
+                    id_=row[0],
+                    activity_end_time=row[1],
+                    archival_group_uri=row[2],
+                    activity_type=row[3],
+                    id_service_pid=row[4],
+                    catalogue_api_uri=row[5],
+                    public_manifest_uri=row[6],
+                    internal_public_manifest_uri=row[7],
+                    internal_api_manifest_uri=row[8],
+                    started=row[9],
+                    finished=row[10],
+                    error_message=row[11]
+                )
+
+
+    def save(self):
+        with psycopg.connect(settings.POSTGRES_CONNECTION) as conn:
+            with conn.cursor() as cur:
+                sql = ("UPDATE archival_group_activity SET  "
+                       "id_service_pid=%s, catalogue_api_uri=%s, public_manifest_uri=%s, "
+                       "internal_public_manifest_uri=%s, internal_api_manifest_uri=%s, "
+                       "finished=%s, error_message=%s "
+                       "WHERE id = %s")
+                values = (
+                    self.id_service_pid,
+                    self.catalogue_api_uri,
+                    self.public_manifest_uri,
+                    self.internal_public_manifest_uri,
+                    self.internal_api_manifest_uri,
+                    self.finished,
+                    self.error_message,
+                    self.id
+                )
+                cur.execute(sql, values)
+
+
+
+# create table archival_group_activity
+# (
+#     id                           serial primary key,
+#     activity_end_time            timestamp not null,
+#     archival_group_uri           text      not null,
+#     activity_type                text      not null,
+#     id_service_pid               text,
+#     catalogue_api_uri            text,
+#     public_manifest_uri          text,
+#     internal_public_manifest_uri text,
+#     internal_api_manifest_uri    text,
+#     started                      timestamp not null,
+#     finished                     timestamp,
+#     error_message                text
+# );
+#
+# alter table archival_group_activity
+#     owner to postgres;
--- src/iiif-builder/app/iiif_builder.py
+++ src/iiif-builder/app/iiif_builder.py
@@ -5,7 +5,7 @@ import app.settings as settings
 from logzero import logger
 
 from signal_handler import SignalHandler
-from app.db import db, ArchivalGroupActivity
+from app.db import ArchivalGroupActivity
 from preservation_api import get_activities, load_archival_group, load_mets
 from identity_service import get_identities_from_archival_group, get_internal_iiif_uris
 from catalogue_api import read_catalogue_api
@@ -16,8 +16,6 @@ from iiif_cloud_services import put_manifest
 
 async def read_stream():
     logger.info("starting iiif-builder...")
-    await db.set_bind(settings.POSTGRES_CONNECTION)
-
     signal_handler = SignalHandler()
 
     try:
@@ -41,75 +39,77 @@ async def read_stream():
 
 async def process_activity(activity, session):
 
-    archival_group_uri = activity["object"]["id"]
-    stored = ArchivalGroupActivity.create(
+    job: ArchivalGroupActivity = ArchivalGroupActivity.new_activity(
         activity_end_time = activity["endTime"],
-        archival_group_uri = archival_group_uri,
-        activity_type = activity["type"],
-        started = datetime.now(datetime.timezone.utc)
+        archival_group_uri = activity["object"]["id"],
+        activity_type = activity["type"]
     )
 
-    logger.debug(f"Loading archival group from {archival_group_uri}")
-    archival_group_result = await load_archival_group(session, archival_group_uri)
+    logger.debug(f"Loading archival group from {job.archival_group_uri}")
+    archival_group_result = await load_archival_group(session, job.archival_group_uri)
     if archival_group_result.failure:
         logger.error(f"Failed to load archival group: {archival_group_result.error}")
-        await stored.update(error_message=archival_group_result.error).apply()
+        job.error_message = archival_group_result.error
+        job.save()
         return
 
-    logger.debug(f"Loading METS for archival group {archival_group_uri}")
-    mets_result = await load_mets(session, archival_group_uri)
+    logger.debug(f"Loading METS for archival group {job.archival_group_uri}")
+    mets_result = await load_mets(session, job.archival_group_uri)
     if mets_result.failure:
         logger.error(f"Failed to load METS for archival group: {mets_result.error}")
-        await stored.update(error_message=mets_result.error).apply()
+        job.error_message = mets_result.error
+        job.save()
         return
 
-    logger.debug(f"Calling identity service for archival group {archival_group_uri}")
+    logger.debug(f"Calling identity service for archival group {job.archival_group_uri}")
 
-    identities_result = await get_identities_from_archival_group(session, archival_group_uri)
+    identities_result = await get_identities_from_archival_group(session, job.archival_group_uri)
     if identities_result.failure:
-        logger.error(f"Failed to get Identities for archival group{archival_group_uri}: {identities_result.error}")
-        await stored.update(error_message=identities_result.error).apply()
+        logger.error(f"Failed to get Identities for archival group{job.archival_group_uri}: {identities_result.error}")
+        job.error_message = identities_result.error
+        job.save()
         return
 
-    pid = identities_result.value["pid"]
+    job.id_service_pid = identities_result.value["pid"]
     if settings.USE_MVP_CATALOGUE_API:
-        catalogue_api_uri = f"{settings.MVP_CATALOGUE_API_PREFIX}{pid}"
+        job.catalogue_api_uri = f"{settings.MVP_CATALOGUE_API_PREFIX}{job.id_service_pid}"
     else:
-        catalogue_api_uri = identities_result.value["catalogue_api_uri"]
+        job.catalogue_api_uri = identities_result.value["catalogue_api_uri"]
 
     internal_iiif_uris = get_internal_iiif_uris(identities_result.value["manifest_uri"])
-    internal_public_manifest_uri = internal_iiif_uris["public_manifest_uri"]
-    internal_api_manifest_uri = internal_iiif_uris["api_manifest_uri"]
-    await stored.update(
-        catalogue_api_uri=catalogue_api_uri,
-        public_manifest_uri=internal_public_manifest_uri,
-        api_manifest_uri=internal_api_manifest_uri).apply()
+    job.internal_public_manifest_uri = internal_iiif_uris["public_manifest_uri"]
+    job.internal_api_manifest_uri = internal_iiif_uris["api_manifest_uri"]
+    job.save()
 
-    logger.debug(f"Getting descriptive metadata from catalogue API for {catalogue_api_uri}")
-    descriptive_metadata_result = await read_catalogue_api(session, catalogue_api_uri)
+    logger.debug(f"Getting descriptive metadata from catalogue API for {job.catalogue_api_uri}")
+    descriptive_metadata_result = await read_catalogue_api(session, job.catalogue_api_uri)
     if descriptive_metadata_result.failure:
         logger.error(f"Failed to load descriptive metadata from catalogue API: {descriptive_metadata_result.error}")
-        await stored.update(error_message=descriptive_metadata_result.error).apply()
+        job.error_message = descriptive_metadata_result.error
+        job.save()
         return
 
     manifest = get_boilerplate_manifest()
     add_descriptive_metadata_to_manifest(manifest, descriptive_metadata_result.value)
 
-    logger.debug(f"Adding painted resources to manifest {internal_public_manifest_uri}")
+    logger.debug(f"Adding painted resources to manifest {job.internal_public_manifest_uri}")
     add_painted_resources_result = add_painted_resources(manifest, archival_group_result.value, mets_result.value)
     if add_painted_resources_result.failure:
         logger.error(f"Failed to add painted resources to Manifest: {add_painted_resources_result.error}")
-        await stored.update(error_message=add_painted_resources_result.error).apply()
+        job.error_message = add_painted_resources_result.error
+        job.save()
         return
 
-    logger.debug(f"Saving Manifest to IIIF-CS: {internal_public_manifest_uri}")
-    put_manifest_result = put_manifest(internal_api_manifest_uri, manifest)
+    logger.debug(f"Saving Manifest to IIIF-CS: {job.internal_public_manifest_uri}")
+    put_manifest_result = put_manifest(job.internal_api_manifest_uri, manifest)
     if put_manifest_result.failure:
         logger.error(f"Failed to PUT Manifest to IIIF-CS: {put_manifest_result.error}")
-        await stored.update(error_message=put_manifest_result.error).apply()
+        job.error_message = put_manifest_result.error
+        job.save()
         return
 
-    await stored.update(finished=datetime.now(datetime.timezone.utc)).apply()
+    job.finished = datetime.now(datetime.timezone.utc)
+    job.save()
 
 
 
