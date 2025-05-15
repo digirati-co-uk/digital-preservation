9677dcf2e10a6bd798080d731d7c5057fda380e1
tomcrane <tom@nanoplanet.com>
2025-05-15 18:19:58 +0100

    emit some settings on status page

--- src/DigitalPreservation/DigitalPreservation.UI/Pages/Status.cshtml.cs
+++ src/DigitalPreservation/DigitalPreservation.UI/Pages/Status.cshtml.cs
@@ -2,14 +2,30 @@
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Options;
+using Preservation.Client;
 using Storage.Repository.Common;
 using Storage.Repository.Common.Requests;
 
 namespace DigitalPreservation.UI.Pages;
 
-public class StatusModel(IMediator mediator) : PageModel
+public class StatusModel(
+    IOptions<PreservationOptions> preservationOptions,
+    IOptions<AwsStorageOptions> awsStorageOptions,
+    IMediator mediator) : PageModel
 {
     [BindProperty] public List<ConnectivityCheckResult> ConnectivityChecks { get; set; } = [];
+
+    public Dictionary<string, string> GetOptions()
+    {
+        var presOptions = preservationOptions.Value;
+        var storageOptions = awsStorageOptions.Value;
+        return new Dictionary<string, string>()
+        {
+            ["Preservation__Root"] = presOptions.Root.ToString(),
+            ["AwsStorage__DefaultWorkingBucket"] = storageOptions.DefaultWorkingBucket
+        };
+    }
     
     public async Task OnGet()
     {
