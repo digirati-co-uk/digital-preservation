328b40d7c4ff8e477d0b5466781dc75c48e351ce
brianmac1967 <brian.mac1967.00@gmail.com>
2025-09-04 15:11:11 +0100

    Required attribute for the Id property in model

--- src/DigitalPreservation/DigitalPreservation.Common.Model/PipelineApi/PipelineDeposit.cs
+++ src/DigitalPreservation/DigitalPreservation.Common.Model/PipelineApi/PipelineDeposit.cs
@@ -2,7 +2,7 @@
 namespace DigitalPreservation.Common.Model.PipelineApi;
 public class PipelineDeposit
 {
-    public string Id { get; set; }
+    public required string Id { get; set; }
     public string? DepositId { get; set; }
     public string Status { get; set; }
     public string? RunUser { get; set; }
