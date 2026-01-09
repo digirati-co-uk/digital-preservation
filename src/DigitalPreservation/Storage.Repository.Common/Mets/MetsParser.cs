1dc6ee782b83d9fa8cc23d93aa0588e7b375c8ab
brianmac1967 <brian.mac1967.00@gmail.com>
2026-01-09 10:56:02 +0000

    Patch the virus definition in METS with the deposit dvirus definition

--- src/DigitalPreservation/Storage.Repository.Common/Mets/MetsParser.cs
+++ src/DigitalPreservation/Storage.Repository.Common/Mets/MetsParser.cs
@@ -589,19 +589,9 @@ public class MetsParser(
                     };
                 }
 
-                XElement? digiprovMd = null;
-
-                try
-                {
-                    digiprovMd = xMets.Descendants(XNames.MetsDigiprovMD).FirstOrDefault(t =>
+                var digiprovMd = xMets.Descendants(XNames.MetsDigiprovMD).FirstOrDefault(t =>
                         t.Attribute("ID")!.Value.ToLower()
                             .Contains($"digiprovmd_clamav_{admId.ToLower()}")); //TODO:working file
-                }
-                catch (Exception e)
-                {
-                    logger.LogError($"object id digiprovmd_clamav_{admId.ToLower()} error {e.Message}");
-                }
-
 
                 var virusEvent = digiprovMd?.Descendants(XNames.PremisEvent).SingleOrDefault();
                 if (virusEvent != null)
